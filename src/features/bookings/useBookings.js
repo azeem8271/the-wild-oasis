20ac1f6213da2219be0cb8f25ff28531e411f949
azeem8271 <azeemkhan07082000@gmail.com>
2023-08-06 18:22:45 +0530

    feat: provide filter and sort options for booking table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,29 @@
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 import { useQuery } from "@tanstack/react-query";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //Filter list
+  const filteredValue = searchParams.get("status");
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : { field: "status", value: filteredValue };
+
+  //Sort list
+  const sortByOrder = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByOrder.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
